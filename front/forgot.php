38ba72686ab6ff218f0ff8761b67f534966b9831
Rita <qazasd1011@gmail.com>
2025-07-14 15:26:30 +0800

    忘記密碼

--- front/forgot.php
+++ front/forgot.php
@@ -2,9 +2,17 @@
     <legend>忘記密碼</legend>
     <div>請輸入信箱以查詢密碼</div>
     <div>
-        <input type="text" name="email">
+        <input type="text" name="email" id="email">
     </div>
+    <div id="ans"></div>
     <div>
-        <input type="button" value="尋找">
+        <button onclick="search()">尋找</button>
     </div>
 </fieldset>
+<script>
+function search(){
+    $.get("./api/chk_email.php",{email:$('#email').val()},(res)=>{
+            $('#ans').html(res);
+    })
+}
+</script>
